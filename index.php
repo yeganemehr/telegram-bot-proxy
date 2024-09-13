a1bca35b6ad50a257511229e946408513de014b1
Amir H. Yeganemehr <ygnmehr@gmail.com>
2024-09-13 21:34:45 +0330

    Handle POST requests

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-$allowedBots = explode(",", getenv("ALLOWED_BOTS") ?: "");
+$allowedBots = trim(getenv("ALLOWED_BOTS") ?: "");
+$allowedBots = $allowedBots ? explode(",", $allowedBots) : [];
 
 if ($allowedBots and (!preg_match("/^\/bot([^\/]+)\//", $_SERVER['REQUEST_URI'], $matches) or !in_array($matches[1], $allowedBots))) {
-	header("HTTP/1.1 403 Forbiden");
+	header("{$_SERVER['SERVER_PROTOCOL']} 403 Forbiden");
 	echo "Access denied";
 	return;
 }
@@ -12,32 +13,45 @@ $client = curl_init("https://api.telegram.org" . $_SERVER['REQUEST_URI']);
 curl_setopt($client, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
 curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($client, CURLOPT_HEADER, false);
-curl_setopt($client, CURLOPT_HEADERFUNCTION, function($client, $header) {
+curl_setopt($client, CURLOPT_HEADERFUNCTION, function ($client, $header) {
 	$len = strlen($header);
 	if (str_ends_with($header, "\r\n")) {
 		$header = substr($header, 0, -2);
 	}
-	$pass = !empty($header);
+	if (empty($header)) {
+		return $len;
+	}
 	$colon = strpos($header, ":");
+	if (!$colon) {
+		if (!preg_match("/^HTTP\/[\d\.]+ (\d+)/", $header, $matches)) {
+			return $len;
+		}
+		$header =  "{$_SERVER['SERVER_PROTOCOL']} {$matches[1]}";
+	}
 	if ($colon) {
 		$key = substr($header, 0, $colon);
 		$key = strtolower($key);
 		if (in_array($key, ["date", "server"])) {
-			$pass = false;
+			return $len;
 		}
 	}
-	if ($pass) {
-		header($header);
-	}
+	header($header);
 
 	return $len;
 });
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	curl_setopt($client, CURLOPT_POSTFIELDS, file_get_contents('php://input'));
+	if (isset($_SERVER['HTTP_CONTENT_TYPE'])) {
+		curl_setopt($client, CURLOPT_HTTPHEADER, [
+			"Content-Type: " . $_SERVER['HTTP_CONTENT_TYPE'],
+		]);
+	}
+}
 $result = curl_exec($client);
 $info = curl_getinfo($client);
 if (!$info["http_code"]) {
-	header("HTTP/1.1 500");
+	header("{$_SERVER['SERVER_PROTOCOL']} 500");
 	echo "Cannot connect to upstream";
 	return;
 }
 echo $result;
-
